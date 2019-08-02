68fe62ee43814b1b2a5d4b5b8ea49af639de5b53
风痕 <inkedawn@gmail.com>
2019-08-02 16:34:14 +0800

    贪心A1067 贪心B1020

--- 2算法初步/贪心A1067/贪心A1067.cpp
+++ 2算法初步/贪心A1067/贪心A1067.cpp
@@ -10,63 +10,43 @@ int main()
 {
 	cin.sync_with_stdio(false);
 	int n; cin >> n;
-	vector<int> v(n); 
-	for (int& i : v) {
-		cin >> i;
-	}
-	int izero = 0;
-	// find zero
+	vector<int> pos(n); // 由于保证无重，不实际顺序存储元素，而是采用记录出现的那些元素的对应位置的方法。
+	// 在模拟交换时，可以直接O(1)直接查到元素所在位置。
+	// 反之，如果存储元素，则需要遍历查找。
 	for (int i = 0; i < n; i++) {
-		if (v[i] == 0) {
-			izero = i;
-			break;
-		}
+		cin >> pos[i];
 	}
-	int k = 0;
-	// find first not sorted
+	int k; // 作为查找起始点，降低时间复杂度。k至多只会从1到n-1一次。
+	// find the first not sorted as k
 	for (int i = 0; i < n; i++) {
-		if (v[i] != i) {
+		if (pos[i] != i) {
 			k = i;
 			break;
 		}
 	}
 	int ans = 0;
 	while (1) {
-		// check order & update k
-		bool order = true;
-		for (int i = k; i < n; i++) {
-			if (v[i] != i) {
-				k = i;
-				order = false;
-				break;
+		if (pos[0] == 0) {
+			// check order,
+			// if ordered already, terminate the process.
+			// if unordered still, update k(firstly not sorted) so that we can avoid comparing those sorted
+			// and swap zero with it(firstly not sorted) so that sort with swap(0, pos[0]) can continue.
+			while (k < n && pos[k]==k) {
+				k++;
 			}
-		}
-		if (order) {
-			break;
-		}
-		if (v[0] == 0) {
-			for (int i = k; i < n; i++) {
-				// swap zero with any number which is not in its correct position
-				if (v[i] != i) {
-					clog << "swap" << v[0] << " " << v[i] << endl;
-					swap(v[0], v[i]);
-					izero = i; // 注意更新izero
-					ans++;
-					break;
-				}
+			if (k == n) {
+				// all elements have been sorted.
+				break; 
 			}
+			clog << "swap" << pos[0] << " " << pos[k] << endl;
+			swap(pos[0], pos[k]);
 		}
 		else {
-			for (int i = 0; i < n; i++) {
-				if (v[i] != i && v[i] == izero) {
-					clog << "swap" << v[izero] << " " << v[i] << endl;
-					swap(v[izero], v[i]);
-					izero = i; // 注意更新izero
-					ans++;
-					break;
-				}
-			}
+			// sort with swap(0, pos[0])
+			clog << "swap" << pos[0] << " " << pos[pos[0]] << endl;
+			swap(pos[0], pos[pos[0]]);
 		}
+		ans++;
 	}
 	cout << ans << endl;
 }
