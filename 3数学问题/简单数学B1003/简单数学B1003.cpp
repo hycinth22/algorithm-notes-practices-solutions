ce6c8b3ffccec7a6ff6ef64bb6a5a1df3ec9a6c5
风痕 <inkedawn@gmail.com>
2019-08-13 00:40:34 +0800

    3.数学问题

---
+++ 3数学问题/简单数学B1003/简单数学B1003.cpp
@@ -0,0 +1,60 @@
+﻿// 简单数学B1003.cpp
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool f(const string& str) {
+	size_t left = 0, mid = 0, right = 0;
+	size_t i = 0, n = str.size();
+	while (i < n && str[i]=='A') {
+		left++;
+		i++;
+	}
+	if (str[i]!='P') {
+		return false;
+	}
+	i++;
+	while (i < n && str[i] == 'A') {
+		mid++;
+		i++;
+	}
+	if (str[i] != 'T') {
+		return false;
+	}
+	i++;
+	while (i < n && str[i] == 'A') {
+		right++;
+		i++;
+	}
+	if (i!=n) {
+		return false;
+	}
+	while (mid > 0) {
+		// case 1 & 2，注意题意中左右都是x，必须相等
+		if (mid == 1 && left==right) {
+			return true;
+		}
+		// case 3
+		else if (right >= left && mid > 0) {
+			right -= left;
+			mid--;
+		}
+		else {
+			break; // 没有变化，则直接中断（否则死循环）
+		}
+	}
+	return false;
+}
+
+int main()
+{
+	int n; cin >> n; cin.get();
+	while (n--) {
+		string str;
+		getline(cin, str);
+		cout << (f(str) ? "YES" : "NO") << endl;
+	}
+}
+
---
+++ 3数学问题/简单数学B1019_A1069/简单数学B1019_A1069.cpp
@@ -0,0 +1,49 @@
+﻿// 简单数学B1019_A1069.cpp
+//
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+
+string sub(const string& str1, const string& str2) {
+	string result = to_string(stoi(str1) - stoi(str2));
+	size_t len = result.length();
+	if (len < 4) {
+		result.insert(0, 4-len, '0');
+	}
+	return result;
+}
+
+bool euqal4(const string& str) {
+	return str.size() == 4 && str[0] == str[1] && str[1] == str[2] && str[2] == str[3];
+}
+
+int main()
+{
+	string str; getline(cin, str);
+	size_t len = str.size();
+	if (len > 4) {
+		return 1;
+	}
+	if (len < 4) { 
+		// 注意，题目没有说明是4位字符串，只说是正整数。小于4位的输入是合法数据，需要手动补齐4位
+		str.insert(0, 4-len, '0');
+	}
+	if (euqal4(str)) {
+		cout << str << " - " << str << " = " << "0000" << endl;
+		return 0;
+	}
+	string str2(str), result;
+	while (result != "6174") {
+		sort(str.begin(), str.end(), greater<char>());
+		sort(str2.begin(), str2.end(), less<char>());
+		result = sub(str, str2);
+		cout << str << " - " << str2 << " = " << result << endl;
+		str = str2 = result;
+	}
+}
+
