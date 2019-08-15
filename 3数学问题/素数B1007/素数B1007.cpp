206991457a4cac63a41df6e5ebc4db629282f3fc
风痕 <inkedawn@gmail.com>
2019-08-15 17:03:02 +0800

    素数表算法优化

--- 3数学问题/素数B1007/素数B1007.cpp
+++ 3数学问题/素数B1007/素数B1007.cpp
@@ -8,31 +8,27 @@
 #include <map>
 using namespace std;
 
-template<typename NumType = unsigned long long>
+// Eratosthenes筛法创建素数表
+template<typename NumType>
 struct primetable {
 	vector<NumType> primes;
-	map<NumType, bool> filter; //	Eratosthenes筛法, true代表已判断不是素数
-	void findUtil(NumType n) {
-		if (n < filter.size()) {
-			return;
-		}
-		clear();
-		// neither 0 or 1 is not a prime. 2 is a prime .
-		filter[0] = true;
-		filter[1] = true;
-		filter[2] = false;
-		for (NumType i = 2; i <= n; i++) {
-			if (!filter[i]) {
+	primetable(NumType maxN) {
+		vector<bool> isPrime{ false, false, true }; // neither 0 or 1 is not a prime. 2 is a prime .
+		isPrime.resize(maxN + 1, true); // zero also need a space
+		for (NumType i = 2; i <= maxN; i++) {
+			if (isPrime[i]) {
 				primes.push_back(i);
-				for (NumType j = i*2; j <= n; j += i) {
-					filter[j] = true;
+				for (NumType j = 2 * i; j <= maxN; j += i) {
+					isPrime[j] = false;
 				}
 			}
 		}
 	}
-	void clear() {
-		primes.clear();
-		filter.clear();
+	NumType operator[](size_t index) const {
+		return primes[index];
+	}
+	size_t size() const {
+		return primes.size();
 	}
 };
 
@@ -40,11 +36,10 @@ int main()
 {
 
 	int n; cin >> n;
-	primetable<> t;
-	t.findUtil(n);
+	primetable<int> t(n);
 	int ans = 0;
-	for (size_t i = 1; i < t.primes.size(); i++) {
-		if (t.primes[i] - t.primes[i - 1] == 2) {
+	for (size_t i = 1; i < t.size(); i++) {
+		if (t[i] - t[i - 1] == 2) {
 			ans++;
 		}
 	}
--- 3数学问题/素数B1013/素数B1013.cpp
+++ 3数学问题/素数B1013/素数B1013.cpp
@@ -2,49 +2,56 @@
 // 素数表方法适用于对某一段密集的素数进行处理，要运算的素数间有关联。
 // 如此题需要输出的素数是按素数序号进行输出，可以减少验证素数的重复计算。
 // 缺点是要必须得知输入最大的数字，以及如果不使用bit表（bitset或vector<bool>）可能占用过多空间。
-
 #include <iostream>
 #include <vector>
+#include <cmath>
 using namespace std;
 
-
-template<typename NumType = unsigned long long>
-struct primetable{
+// Eratosthenes筛法创建素数表
+template<typename NumType>
+struct primetable {
 	vector<NumType> primes;
-	vector<bool> filter; //	Eratosthenes筛法, true代表已判断不是素数
-	// TODO: 不完善的地方。 
-	// 题目只给出了要求第几个素数，而我无法得知第n个素数在什么数值范围，
-	// 因而此处需要手动调maxN（最大能判断出的素数大小），数值越大，消耗内存和分配相应内存时间也越大
-	template<NumType maxN = 10000000>
-	void findNth(size_t n) {
-		if (n < primes.size()) {
-			return;
-		}
-		clear();
-		filter = { true, true, false }; // neither 0 or 1 is not a prime. 2 is a prime 
-		filter.resize(maxN);
-		for (NumType i = 2; primes.size()<n; i++) {
-			if (!filter[i]) {
+	primetable(NumType maxN) {
+		generate(maxN);
+	}
+	void generate(NumType maxN) {
+		primes.clear();
+		vector<bool> isPrime{ false, false, true }; // neither 0 or 1 is not a prime. 2 is a prime .
+		isPrime.resize(maxN + 1, true); // zero also need a space
+		for (NumType i = 2; i <= maxN; i++) {
+			if (isPrime[i]) {
 				primes.push_back(i);
-				for (NumType j = i*2; j < maxN; j+=i) {
-					filter[j] = true;
+				for (NumType j = 2 * i; j <= maxN; j += i) {
+					isPrime[j] = false;
 				}
 			}
 		}
 	}
-	void clear() {
-		primes.clear();
-		filter.clear();
+	NumType operator[](size_t index) const {
+		return primes[index];
+	}
+	size_t size() const {
+		return primes.size();
 	}
 };
+
 
 int main()
 {
 	int n, m; cin >> m >> n;
-	primetable<> t;
-	t.findNth(n);
-	for (size_t i = m; i <= n; i++) {
-		cout << t.primes[i-1];
+	primetable<unsigned long long> t(0);
+	// 题目只给出了要求第几个素数，而我无法得知第n个素数在什么数值范围，
+	// 此处自动探查扩增素数表，即使这样会造成少量重复运算，仍然比逐个判断高效太多。
+	{
+		int maxN = 10000; //初值
+		while (t.size() < n) {
+			maxN *= 30; // 30为底的指数扩增
+			t.generate(maxN);
+		}
+	}
+
+	for (int i = m; i <= n; i++) {
+		cout << t[i-1];
 		if ((i - m + 1) % 10 == 0) {
 			cout << endl;
 		}
