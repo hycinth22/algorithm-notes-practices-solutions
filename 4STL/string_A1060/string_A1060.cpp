378d989c4cec8860d67e9b24109cd2e192fc51fa
风痕 <inkedawn@gmail.com>
2019-08-18 19:30:16 +0800

    A1060修正

--- 4STL/string_A1060/string_A1060.cpp
+++ 4STL/string_A1060/string_A1060.cpp
@@ -5,9 +5,9 @@
 #include <string>
 using namespace std;
 
-string normalize(const string& str, int n) {
+string normalize(const string& str, size_t n) {
 	string normal = "0.";
-	size_t i = 0, len = str.size(), 
+	size_t i = 0, len = str.size(), // str遍历
 		cnt = 0; // 已复制的有效数字位数，最多为n
 	int exp = 0; // 指数
 	// 整数部分开始
@@ -17,7 +17,7 @@ string normalize(const string& str, int n) {
 	}
 	// 统计整数位数（cnt）、指数（exp），复制最多n个有效数字
 	while (i < len && str[i] != '.') {
-		if (exp < n) {
+		if (cnt < n) {
 			normal.append(1, str[i]);
 		}
 		exp++;
@@ -48,7 +48,7 @@ string normalize(const string& str, int n) {
 	}
 	if (cnt == 0) {
 		exp = 0; // 特例，如果没有有效数字（结果是0.0...0），指数需要归0
-		//（否则输出指数为负数，由前面skip prefix zero部分生成）
+		//（否则输出指数为负数，对应前面skip prefix zero部分遍历的0个数）
 	}
 	if (cnt < n) {
 		normal.append(n - cnt, '0'); // 有效数字不足，剩余的补0
