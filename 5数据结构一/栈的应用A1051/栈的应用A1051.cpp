ef1a84280d000e10fce826deb4e3e94a86350813
风痕 <inkedawn@gmail.com>
2019-09-24 19:14:19 +0800

    栈的应用 队列的应用 A1051 A1056

---
+++ 5数据结构一/栈的应用A1051/栈的应用A1051.cpp
@@ -0,0 +1,42 @@
+﻿// 栈的应用A1051.cpp
+//
+
+#include <iostream>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+	int m, n, k; cin >> m >> n >> k;
+	while (k--) {
+		stack<int> s; // 数据栈
+		queue<int> q; // 输入的尝试模拟序列
+		for (int i = 1; i <= n; i++) {
+			int t; cin >> t;
+			q.push(t);
+		}
+		// 模拟弹出。i是输入序列，尝试拟合q
+		for (int i=1; i<=n; i++) {
+			if (s.size() < m) {
+				s.push(i);
+			}
+			else {
+				goto no;
+			}
+			// pop randomly -> 尝试拟合q
+			while (!s.empty() && !q.empty() && s.top() == q.front()) {
+				s.pop(); q.pop(); // acknowledge it
+			}
+		}
+		if (q.empty()) {
+			cout << "YES" << endl;
+		}
+		else {
+			no:
+			cout << "NO" << endl;
+		}
+	}
+}
+
---
+++ 5数据结构一/队列的应用A1056/队列的应用A1056.cpp
@@ -0,0 +1,61 @@
+﻿// 队列的应用A1056.cpp
+//
+
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	unsigned np, ng; cin >> np >> ng;
+	vector<unsigned> weights(np);
+	vector<unsigned> ranks(np);
+	for (auto& w : weights) {
+		cin >> w;
+	}
+	queue<size_t> q;
+	for (size_t i = 0; i < np; i++) {
+		size_t id; cin >> id;
+		q.push(id);
+	}
+	unsigned rank = np+1;
+	while (q.size() > 1) {
+		// new turn
+		clog << "----new turn ---" << endl;
+		clog << "the turn has " << q.size() << " players" << endl;
+		// calc the loser rank
+		unsigned nGroup = ((q.size()%ng) ? (q.size() / ng + 1) : (q.size() / ng));
+		clog << nGroup << " groups" << endl;
+		unsigned nLosers = q.size()- nGroup; // the number of winners is same as the group
+		clog << nLosers << " losers" << endl;
+		rank -= nLosers;
+		clog << rank << " rank" << endl;
+
+		// process every group
+		queue<size_t> nextTurn;
+		while (!q.empty()) {
+			// pop a group, assign all rank and find the winner
+			size_t winner = q.front();
+			for (size_t i = 0; i < ng && !q.empty(); i++) {
+				auto id = q.front(); q.pop();
+				if (weights[id] > weights[winner]) {
+					clog << id << "win" << winner << endl;
+					winner = id;
+				}
+				ranks[id] = rank;
+			}
+			nextTurn.push(winner);
+			clog << "winner" << winner << endl;
+		}
+		// over a turn
+		q.swap(nextTurn);
+	}
+	ranks[q.front()] = 1;
+	cout << ranks.front();
+	for (size_t i = 1; i < np; i++) {
+		cout << " " << ranks[i];
+	}
+	cout << endl;
+}
+
