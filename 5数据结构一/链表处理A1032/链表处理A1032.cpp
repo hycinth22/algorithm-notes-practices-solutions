8c6c2b29607ac9ceec77d2f5570cfb4a318f03a0
风痕 <inkedawn@gmail.com>
2019-09-26 19:48:01 +0800

    链表处理
    A1032
    A1052

---
+++ 5数据结构一/链表处理A1032/链表处理A1032.cpp
@@ -0,0 +1,48 @@
+﻿// 链表处理A1032.cpp
+//
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+struct node {
+	char data;
+	int next;
+};
+
+int main()
+{
+	unordered_map<int, node> m;
+	int head1, head2; scanf("%d%d", &head1, &head2);
+	int n; scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		int addr; static node n;  scanf("%5d %c %5d", &addr, &n.data, &n.next);
+		m.insert(make_pair<>(addr, n));
+	}
+	vector<int> seq1; seq1.reserve(n);
+	int c = head1;
+	while (c != -1) {
+		seq1.push_back(c);
+		c = m.at(c).next;
+	}
+	vector<int> seq2; seq2.reserve(n);
+	c = head2;
+	while (c != -1) {
+		seq2.push_back(c);
+		c = m.at(c).next;
+	}
+	int ir = -1;
+	for (auto i1 = seq1.rbegin(), i2 = seq2.rbegin(); i1 != seq1.rend() && i2 != seq2.rend(); i1++, i2++) {
+		if (*i1 == *i2) {
+			ir = *i1;
+		}
+	}
+	if (ir != -1) {
+		printf("%05d", ir);
+	}
+	else {
+		printf("-1");
+	}
+}
+
---
+++ 5数据结构一/链表处理A1052/链表处理A1052.cpp
@@ -0,0 +1,50 @@
+﻿// 链表处理A1052.cpp
+//
+
+#include <iostream>
+#include <cstdio>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct node {
+	int key;
+	int next;
+};
+
+int main()
+{
+	int n, head; cin >> n >> head;
+	unordered_map<int, node> m;
+	for (int i = 0; i < n; i++) {
+		int addr; node t; cin >> addr >> t.key >> t.next;
+		m.insert(make_pair<>(addr, t));
+	}
+	// 根据链表顺序取得所有结点的地址，并线性存储。
+	vector<int> seq; seq.reserve(n);
+	int cur = head;
+	while (cur != -1) {
+		seq.push_back(cur);
+		cur = m.at(cur).next;
+	}
+	sort(seq.begin(), seq.end(), [&](int a, int b) {
+		return m.at(a).key < m.at(b).key;
+		});
+	if (!seq.empty()) {
+		// 注意要取size()而非直接输出n，因为输入节点可能并不在链表中
+		printf("%d %05d\n", seq.size(), seq.front());
+	}
+	else {
+		printf("0 -1\n");
+	}
+	if (!seq.empty()) {
+		// 务必判空，否则size=0时会运行出错。
+		// 因为size()是无符号数，size()为0时size()-1回绕到unsigned最大值，继而访问越界。
+		for (size_t i = 0; i < seq.size() - 1; i++) {
+			printf("%05d %d %05d\n", seq[i], m.at(seq[i]).key, seq[i + 1]);
+		}
+		printf("%05d %d -1\n", seq.back(), m.at(seq.back()).key);
+	}
+}
+
