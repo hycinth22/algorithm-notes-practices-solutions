ac307c36434643a1a1c690d89f9f99106f06eb91
风痕 <inkedawn@gmail.com>
2019-09-27 00:08:26 +0800

    链表处理 栈的应用 A1097 codeup contest 100000605

---
+++ 5数据结构一/栈的应用codeup1982/栈的应用codeup1982.cpp
@@ -0,0 +1,43 @@
+﻿// 栈的应用codeup1982.cpp
+// cid=100000605
+
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+bool valid(const string& str) {
+	stack<char> s;
+	for (const char& c : str) {
+		switch (c) {
+		case '(':
+			s.push(')');
+			break;
+		case '[':
+			s.push(']');
+			break;
+		case '{':
+			s.push('}');
+			break;
+		case ')':
+		case ']':
+		case '}':
+			if (s.empty() || s.top() != c) {
+				return false;
+			}
+			s.pop();
+			break;
+		}
+	}
+	return s.empty();
+}
+
+int main()
+{
+	int n; cin >> n; cin.get();
+	while (n--) {
+		string str; getline(cin ,str);
+		cout << (valid(str)?"yes":"no") << endl;
+	}
+}
+
---
+++ 5数据结构一/链表处理A1097/链表处理A1097.cpp
@@ -0,0 +1,51 @@
+﻿// 链表处理A1097.cpp
+//
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int head; int n; cin >> head >> n;
+	unordered_map<int, pair<int, int> > m;
+	for (int i = 0; i < n; i++) {
+		int addr, key, next; cin >> addr >> key >> next;
+		m[addr].first = key;
+		m[addr].second = next;
+	}
+	// 逻辑关系线性化
+	vector<int> seq1 , seq2;
+	unordered_set<int> keys;
+	int cur = head;
+	while (cur != -1) {
+		auto& node = m.at(cur);
+		int key = abs(node.first);
+		if (keys.count(key)) {
+			seq2.push_back(cur);
+		}
+		else {
+			seq1.push_back(cur);
+			keys.insert(key);
+		}
+		cur = m.at(cur).second;
+	}
+	// 输出。注意事项同A1052
+	if (!seq1.empty()) {
+		for (size_t i = 0; i < seq1.size() - 1; i++) {
+			printf("%05d %d %05d\n", seq1[i], m.at(seq1[i]).first, seq1[i+1]);
+		}
+		printf("%05d %d -1\n", seq1.back(), m.at(seq1.back()).first);
+	}
+	if (!seq2.empty()) {
+		for (size_t i = 0; i < seq2.size() - 1; i++) {
+			printf("%05d %d %05d\n", seq2[i], m.at(seq2[i]).first, seq2[i + 1]);
+		}
+		printf("%05d %d -1\n", seq2.back(), m.at(seq2.back()).first);
+	}
+}
+
