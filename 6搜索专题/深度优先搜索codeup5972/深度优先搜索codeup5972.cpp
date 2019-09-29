2a9b05ad80769b0e63215c509d8736ed828d606b
风痕 <inkedawn@gmail.com>
2019-09-29 18:10:02 +0800

    深度优先搜索codeup 100000608 部分

---
+++ 6搜索专题/深度优先搜索codeup5972/深度优先搜索codeup5972.cpp
@@ -0,0 +1,60 @@
+﻿// 深度优先搜索codeup5972.cpp
+// cid=100000608
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void output(const vector<int>& v) {
+	if (!v.empty()) {
+		cout << v.front();
+		for (size_t i = 1; i < v.size(); i++) {
+			cout << " " << v[i];
+		}
+	}
+	cout << endl;
+}
+
+// 静态量：n 范围上限（范围[1, n]）
+// 动态量：ans 当前的全排列，i当前处理的第i位数索引
+int n;
+void dfs(int i, vector<int> ans) {
+	if (i == ans.size()) {
+		output(ans);
+		return;
+	}
+	dfs(i + 1, ans);
+	for (int ci = i+1; ci < ans.size(); ci++) {
+		swap(ans[i], ans[ci]);
+		stable_sort(ans.begin()+i+1, ans.end()); // 字典序
+		dfs(i+1, ans);
+	}
+}
+void traversal_by_dfs(const vector<int>& v) {
+	if (v.empty()) {
+		return;
+	}
+	n = v.back();
+	dfs(0,  v);
+}
+
+// STL提供了字典序遍历全排列的算法next_permutation
+void traversal_by_STL(vector<int>& v) {
+	do {
+		output(v);
+	} while (next_permutation(v.begin(), v.end()));
+}
+
+int main()
+{
+	int n; cin >> n;
+	vector<int> v(n);
+	for (int i = 0; i < n; i++) {
+		v[i] = i+1;
+	}
+	sort(v.begin(), v.end());
+	// traversal_by_STL(v);
+	traversal_by_dfs(v);
+}
+
---
+++ 6搜索专题/深度优先搜索codeup5973/深度优先搜索codeup5973.cpp
@@ -0,0 +1,50 @@
+﻿// 深度优先搜索codeup5973.cpp
+// cid=100000608
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void output(const vector<int>& v) {
+	if (!v.empty()) {
+		cout << v.front();
+		for (size_t i = 1; i < v.size(); i++) {
+			cout << " " << v[i];
+		}
+	}
+	cout << endl;
+}
+
+vector<int> dataset, subset; int r;
+// static: dataset, r
+// dynamic: i for subset filling, j for dataset start, subset
+void dfs(int i, int j) {
+	if (i == r) {
+		output(subset);
+		return;
+	}
+	for (int ci = j; ci < dataset.size(); ci++) {
+		subset[i] = dataset[ci];
+		dfs(i + 1, ci+1);
+	}
+}
+void search(vector<int>& v, int p_r) {
+	r = p_r;
+	dataset = v;
+	subset.resize(r);
+	dfs(0, 0);
+}
+
+int main()
+{
+	int n, r; cin >> n >> r;
+	vector<int> v(n);
+	for (int i = 0; i < n; i++) {
+		v[i] = i + 1;
+	}
+	sort(v.begin(), v.end());
+	search(v, r);
+}
+
+
