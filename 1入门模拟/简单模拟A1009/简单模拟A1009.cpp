0b72848d6906edb37631b3b0bed92647b523358b
风痕 <inkedawn@gmail.com>
2019-10-07 09:03:22 +0800

    支持无限组数据输入

--- 1入门模拟/简单模拟A1009/简单模拟A1009.cpp
+++ 1入门模拟/简单模拟A1009/简单模拟A1009.cpp
@@ -6,32 +6,42 @@
 #include <functional>
 #include <iomanip>
 #include <algorithm>
+#include <cmath>
 using namespace std;
 
 int main()
 {
 	map<int, double, greater<int> > first, result;
-	int n; cin >> n;
+	int n; cin >> n; 
 	while (n--) {
 		int e; double k; cin >> e >> k;
-		first[e] =  k;
+		result[e] =  k;
 	}
-	cin >> n;
-	while (n--) {
-		int e; double k; cin >> e >> k;
-		for (auto it = first.begin(); it != first.end(); it++) {
-			const int& firstE = it->first;
-			const double& firstK = it->second;
-			result[e + firstE] += k* firstK;
+	while (cin >> n) {
+		first = result;
+		result.clear();
+		while (n--) {
+			int e; double k; cin >> e >> k;
+			for (auto it = first.begin(); it != first.end(); it++) {
+				const int& firstE = it->first;
+				const double& firstK = it->second;
+				double t = k * firstK;
+				if (fabs(t)>=0.05) {
+					result[e + firstE] += t;
+				}
+			}
 		}
 	}
+
 	cout << count_if(result.cbegin(), result.cend(), [](auto it) {return it.second != 0.0; });
 	// 注意此处应计算非零项，count_if用法，pred写法
 	cout << fixed << setprecision(1); // 注意保留小数1位
 	for (auto it = result.cbegin(); it != result.cend(); it++) {
 		if (it->second!=0.0) { //注意判断非零
 			cout << " " << it->first << " " << it->second;
+			//cout << " + " << it->second << "x^" << it->first;
 		}
 	}
+	cout << endl;
 }
 
