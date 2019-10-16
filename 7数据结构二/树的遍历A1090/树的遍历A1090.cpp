486a6c9d34e1854d524b40f3228c1ffe51d9fead
风痕 <inkedawn@gmail.com>
2019-10-16 16:24:12 +0800

    树的遍历A1090

---
+++ 7数据结构二/树的遍历A1090/树的遍历A1090.cpp
@@ -0,0 +1,39 @@
+﻿// 树的遍历A1090.cpp
+//
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+struct seller {
+	int supplier;
+	double price;
+};
+void assureCalc(vector<seller> &tree, int i, double r) {
+	if (tree[i].price != -1.00) {
+		return;
+	}
+	if (tree[i].supplier != -1) {
+		assureCalc(tree, tree[i].supplier, r);
+		tree[i].price = tree[tree[i].supplier].price * r;
+	}
+}
+int main()
+{
+	int n; double p, r; scanf("%d %lf %lf", &n, &p, &r);
+	r = r / 100 + 1;
+	vector<seller> all(n);
+	for (int i = 0; i < n; ++i) {
+		scanf("%d", &all[i].supplier);
+		all[i].price = -1.0;
+		if (all[i].supplier == -1) {
+			all[i].price = p;
+		}
+	}
+	for (int i = 0; i < n; ++i) {
+		assureCalc(all, i, r);
+	}
+	double maxprice = max_element(all.cbegin(), all.cend(), [](auto& a, auto& b) {return a.price < b.price; })->price;
+	int cnt = count_if(all.cbegin(), all.cend(), [maxprice](auto& s) {return s.price == maxprice; });
+	printf("%.2f %d", maxprice, cnt);
+}
+
