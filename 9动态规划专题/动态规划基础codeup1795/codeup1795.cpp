9e00d439e539cfa81c0aafb65091ce5ff2f9ace4
风痕 <inkedawn@gmail.com>
2020-02-04 12:54:46 +0800

    codeup 1795

--- 9动态规划专题/动态规划基础codeup1795/codeup1795.cpp
+++ 9动态规划专题/动态规划基础codeup1795/codeup1795.cpp
@@ -4,6 +4,7 @@ using namespace std;
 // r[0]=0
 // r[1]=1
 // r[i] = r[i-2]+r[i-1]
+// 递推写法
 long long fib(size_t n) {
     static vector<long long> r{0, 1};
     r.reserve(n + 1);
@@ -13,10 +14,22 @@ long long fib(size_t n) {
     return r[n];
 }
 
+// 递归写法
+long long fib2(size_t n) {
+    constexpr const int nil = -1;
+    static vector<long long> r{0, 1};
+    if (n < r.size() && r[n] != nil) {
+        return r[n];
+    }
+    r.resize(n + 1, nil);
+    r[n] = fib2(n - 2) + fib2(n - 1);
+    return r[n];
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     int n;
     while (cin >> n) {
-        cout << fib(n) << endl;
+        cout << fib(n) << " | " << fib2(n) << endl;
     }
 }
