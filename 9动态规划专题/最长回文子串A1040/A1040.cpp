8a5a565266f9501c3eb7d7f96ad0c0f5ee468a6b
风痕 <inkedawn@gmail.com>
2020-02-19 13:32:53 +0800

    A1040

--- 9动态规划专题/最长回文子串A1040/A1040.cpp
+++ 9动态规划专题/最长回文子串A1040/A1040.cpp
@@ -5,16 +5,16 @@ size_t lenLSS(const string &str) {
     if (str.empty()) return 0;
     size_t n = str.size();
     vector<vector<bool> > is(n, vector<bool>(n));
-    size_t ans = 1;
+    size_t len = 1;
     // diff=0, ans = 1
     for (size_t i = 0; i < n; ++i) {
         is[i][i] = true;
     }
-    // diff=1, ans = 2
+    // diff=1, ans = 2 (不能统一在后面循环里)
     for (size_t i = 1; i < n; ++i) {
         if (str[i - 1] == str[i]) {
             is[i - 1][i] = true;
-            ans = 2;
+            len = 2;
         }
     }
     // diff>=2, ans=diff+1
@@ -22,12 +22,12 @@ size_t lenLSS(const string &str) {
         for (size_t i = 0, j = i + diff;
              i < n && j < n && i<j; ++i, j = i + diff) {
             if (str[i] == str[j] && is[i + 1][j - 1]) {
-                ans = diff + 1;
+                len = diff + 1;
                 is[i][j] = true;
             }
         }
     }
-    return ans;
+    return len;
 }
 
 int main() {
